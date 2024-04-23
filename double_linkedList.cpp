c89205fa7553b2d7f15b845fd1d9afa81348f417
Habiba-Mohamed <145884551+Habiba-mohammed@users.noreply.github.com>
2024-04-22 20:56:16 -0800

    Update double_linkedList.cpp

--- double_linkedList.cpp
+++ double_linkedList.cpp
@@ -1,228 +1,3 @@
-//
-//#include <iostream>
-//#define ll long long
-//
-//using namespace std;
-//
-//template<typename T>
-//class Node {
-//public:
-//    T data;
-//    Node *next;
-//    Node *prev;
-//    Node() {
-//        next = NULL;
-//        prev = NULL;
-//    }
-//};
-//
-//template<typename T>
-//class double_linkedList {
-//public:
-//    Node<T>* Head;
-//    Node<T>*Last;
-//    double_linkedList() {
-//        Head = NULL;
-//        Last=NULL;
-//    }
-//    bool isEmpty() {
-//        return (Head == NULL);
-//    }
-//
-//    ll size() {
-//        ll size = 0;
-//        if (isEmpty()) {
-//            return size;
-//        }
-//        Node<T>* temp = Head;
-//        while (temp != NULL) {
-//            size++;
-//            temp = temp->next;
-//        }
-//        return size;
-//    }
-//    void append_at_tail(T new_item) {
-//        Node<T>* new_node = new Node<T>();
-//        new_node->data = new_item;
-//        if (isEmpty()) {
-//            new_node->next=NULL;
-//            new_node->prev=NULL;
-//            Head = new_node;
-//            Last=new_node;
-//            return;
-//        }
-//
-//        Node<T>* temp = Head;
-//        while (temp->next != NULL) {
-//            temp = temp->next;
-//        }
-//        temp->next = new_node;
-//        new_node->prev = temp;
-//        Last=new_node;
-//    }
-//    void append_at_first(T new_item) {
-//        if (isEmpty()) {
-//            append_at_tail(new_item);
-//            return;
-//        }
-//        Node<T>* newnode = new Node<T>();
-//        newnode->data = new_item;
-//        newnode->next = Head;
-//        Head->prev = newnode;
-//        Head = newnode;
-//    }
-//    void insert_at_pos(T new_item, ll index) {
-//        if (isEmpty()) {
-//            append_at_tail(new_item);
-//            return;
-//        }
-//        if (index <= 0) {
-//            append_at_first(new_item);
-//            return;
-//        }
-//        if (index >= size()) {
-//            append_at_tail(new_item);
-//            return;
-//        }
-//
-//        ll cur = 0;
-//        Node<T>* temp = Head;
-//        Node<T>* newnode = new Node<T>();
-//        newnode->data = new_item;
-//        while (temp->next != NULL && cur < index) {
-//            cur++;
-//            temp = temp->next;
-//        }
-//        newnode->next=temp;
-//        newnode->prev=temp->prev;
-//        temp->prev->next=newnode;
-//        temp->prev=newnode;
-//
-//    }
-//    void print() {
-//        if (isEmpty()) {
-//            cout << "This list is empty\n";
-//            return;
-//        }
-//        Node<T>* temp = Head;
-//        while (temp != NULL) {
-//            cout << temp->data << " ";
-//            temp = temp->next;
-//        }
-//        cout << '\n';
-//    }
-//    void clear(){
-//        if(!isEmpty()){
-//            Node<T>*temp=Head;
-//            Node<T>*delptr=NULL;
-//            while(temp != NULL){
-//                delptr=temp;
-//                temp=temp->next;
-//                delete delptr;
-//            }
-//           Head =NULL;
-//            Last=NULL;
-//        }
-//    }
-//    bool is_exist(T item){
-//        Node<T>*temp=Head;
-//        while(temp!=NULL){
-//            if(temp->data ==item){
-//                return true;
-//            }
-//
-//            temp=temp->next;
-//        }
-//        return false;
-//
-//    }
-//    bool is_item_equal(T item ,ll index){
-//        ll cur=0;
-//        Node<T>*temp=Head;
-//        while(temp->next!=NULL && cur<index){
-//            temp=temp->next;
-//        }
-//        if(temp->data== item){
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    void remove_tail(){
-//        Node<T>*temp=Head;
-//        Node<T>*delptr=NULL;
-//
-//        delptr=Last;
-//        Last->prev->next=NULL;
-//        delete delptr;
-//    }
-//    void remove_Head(){
-//        Node<T>temp=Head;
-//
-//
-//
-//    }
-//    void remove_at_index(ll index){
-//  ll cur=0;
-//
-//
-//    }
-//};
-//
-//int main() {
-//    double_linkedList<int> myList;
-//    double_linkedList<string>myList_string;
-//
-//    // Appending elements to the list
-//    myList.print();
-//    myList.append_at_tail(10);
-//    myList.print();
-//    myList.append_at_tail(20);
-//    myList.append_at_tail(30);
-//
-//    // Printing the list
-//    cout << "List after appending elements: ";
-//    myList.print(); // Output: 10 20 30
-//
-//    // Inserting at specific positions
-//    myList.insert_at_pos(15, 1);
-//    cout << "List after inserting 15 at position 1: ";
-//    myList.print(); // Output: 10 15 20 30
-//
-//    myList.insert_at_pos(5, 0);
-//    cout << "List after inserting 5 at position 0: ";
-//    myList.print(); // Output: 5 10 15 20 30
-//
-//    myList.insert_at_pos(25, 4);
-//    cout << "List after inserting 25 at position 4: ";
-//    myList.print(); // Output: 5 10 15 20 25 30
-//   cout<< myList.size()<<'\n';
-//  // myList.clear();
-//  //  myList.print();
-//  cout<<myList.is_exist(30)<<'\n';
-//  cout<<myList.is_exist(5)<<'\n';
-//  cout<< myList.is_item_equal(5,0)<<'\n';
-//  myList_string.append_at_tail("Habiba");
-//    myList_string.append_at_tail("Abdelaziz");
-//    myList_string.append_at_tail("baba");
-//    myList_string.append_at_tail("mama");
-//    myList_string.append_at_tail("rahem");
-//    myList_string.append_at_tail("roiaa");
-//    myList_string.print();
-//    myList.remove_tail();
-//    myList.print();
-//    myList.remove_tail();
-//    myList.remove_tail();
-//    myList.remove_tail();
-//    myList.print();
-//
-//
-//
-//
-//}
-//
-//
-//
 #include <iostream>
 #define ll long long
 
@@ -253,8 +28,8 @@ public:
         return (Head == NULL);
     }
 
-    ll size() {
-        ll size = 0;
+    long long size() {
+        long long size = 0;
         if (isEmpty()) {
             return size;
         }
@@ -291,7 +66,7 @@ public:
         Head->prev = newnode;
         Head = newnode;
     }
-    void insert_at_pos(T new_item, ll index) {
+    void insert_at_pos(T new_item, long long index) {
         if (isEmpty()) {
             append_at_tail(new_item);
             return;
@@ -305,7 +80,7 @@ public:
             return;
         }
 
-        ll cur = 0;
+        long long cur = 0;
         Node<T>* temp = Head;
         Node<T>* newnode = new Node<T>();
         newnode->data = new_item;
@@ -353,8 +128,8 @@ public:
         }
         return false;
     }
-    bool is_item_equal(T item, ll index) {
-        ll cur = 0;
+    bool is_item_equal(T item, long long index) {
+        long long cur = 0;
         Node<T>* temp = Head;
         while (temp != NULL && cur < index) {
             temp = temp->next;
@@ -384,24 +159,29 @@ public:
 
         if (isEmpty())
             return;
-        Node<T>*delptr=Head;
-        Head=Head->next;
-        Head->prev=NULL;
+
+        Node<T>* delptr = Head;
+        Head = Head->next;
+        if (Head != NULL)
+            Head->prev = NULL;
         delete delptr;
-//
-//        Node<T>* delptr = Head;
-//        Head = Head->next;
-//        if (Head != NULL)
-//            Head->prev = NULL;
-//        delete delptr;
     }
 
-    void remove_at_index(ll index) {
+    void remove_at_index(long long index) {
         if (isEmpty())
             return;
+        if (index <= 0) {
+            remove_Head();
+            return;
+        }
+        if (index >= size()-1) {
+            remove_tail();
+            return;
+        }
 
-        ll cur = 0;
+        long long cur = 0;
         Node<T>* temp = Head;
+
         while (temp != NULL && cur < index) {
             temp = temp->next;
             cur++;
@@ -411,10 +191,8 @@ public:
             return; // index out of bounds
 
         Node<T>* delptr = temp;
-        if (temp->prev != NULL)
-            temp->prev->next = temp->next;
-        if (temp->next != NULL)
-            temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
         delete delptr;
     }
 };
@@ -424,44 +202,17 @@ int main() {
     double_linkedList<string> myList_string;
 
     // Appending elements to the list
-    myList.print();
     myList.append_at_tail(10);
-    myList.print();
     myList.append_at_tail(20);
     myList.append_at_tail(30);
+    myList.append_at_tail(30);
+    myList.append_at_tail(30);
+    myList.append_at_tail(30);
+    myList.remove_at_index(5);
 
     // Printing the list
     cout << "List after appending elements: ";
     myList.print(); // Output: 10 20 30
 
-    // Inserting at specific positions
-    myList.insert_at_pos(15, 1);
-    cout << "List after inserting 15 at position 1: ";
-    myList.print(); // Output: 10 15 20 30
-
-    myList.insert_at_pos(5, 0);
-    cout << "List after inserting 5 at position 0: ";
-    myList.print(); // Output: 5 10 15 20 30
-
-    myList.insert_at_pos(25, 4);
-    cout << "List after inserting 25 at position 4: ";
-    myList.print(); // Output: 5 10 15 20 25 30
-    cout << myList.size() << '\n';
-
-    cout << myList.is_exist(30) << '\n';
-    cout << myList.is_exist(5) << '\n';
-    cout << myList.is_item_equal(5, 0) << '\n';
-
-    myList_string.append_at_tail("Habiba");
-    myList_string.remove_Head();
-  //  myList_string.print();
-
-    myList_string.append_at_tail("Abdelaziz");
-    myList_string.append_at_tail("baba");
-    myList_string.append_at_tail("mama");
-    myList_string.append_at_tail("rahem");
-    myList_string.append_at_tail("roiaa");
-    myList_string.remove_Head();
-
-    myList_string.print();
+    return 0;
 }
